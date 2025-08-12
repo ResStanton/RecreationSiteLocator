2a28d0fb85c554333db7502a833ed9a7bbe55bf8
Reston <rstanton1@students.ccu.edu>
2025-08-12 14:29:51 -0600

    Added basic logic and error passthrough

--- src/main.py
+++ src/main.py
@@ -16,6 +16,9 @@ db = Database(CONNECTION_STRING, "USFS_recreation_opportunities", "locations")
 # Create basic home page
 @app.route('/')
 def home_page():
+    # create error variable in case an error needs to be displayed 
+    error = ""
+
     # Search bar setup
     search_input = {
         "search_filter": "", 
@@ -48,25 +51,25 @@ def home_page():
         try:
             address_location = geolocate.get_location_from_address(search_input["query"])
         except AttributeError:
-            pass
+            error = r"That Address Could Not Be Found.\nMake sure yoy spelled everything correctly and try again.\nAlso try removing the city from the address"
         except TimeoutError:
-            pass
+            error = r"Request Timed Out.\nPlease try again later."
         else:
             print(address_location)
         locations = db.query_all()
-    elif search_input["search_type"] == "Name":
+    else:
         # Determine what query to use 
         if search_input["query"] and search_input["search_filter"]:
             # Search by name while filtering by activity type
             locations = db.query_name_and_activity_type(search_input["query"], search_input["search_filter"])
         elif search_input["query"]:
             locations = db.query_by_name(search_input["query"])
-    elif search_input["search_filter"]:
-        # Code for searching by activity selected in search bar
-        locations = db.query_by_activity_type(search_input["search_filter"])
-    else:
-        # Get all locations from the database
-        locations = db.query_all()
+        elif search_input["search_filter"]:
+            # Code for searching by activity selected in search bar
+            locations = db.query_by_activity_type(search_input["search_filter"])
+        else:
+            # Get all locations from the database
+            locations = db.query_all()
     
     # loop thought locations to skip broken data and only include the required data 
     markers = []
@@ -90,7 +93,8 @@ def home_page():
         search_input=search_input,
         search_types=search_types,
         search_filters=search_filters,
-        markers=markers
+        markers=markers,
+        error=error
     )
 
 
